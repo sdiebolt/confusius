468589e6fc61eb2a8f58f929cf726ca124d1458e
Felipe Cybis Pereira <41338087+FelipeCybis@users.noreply.github.com>
2026-04-06 02:43:47 +0200

    chore: run tests on Windows (Github Actions) (#43)
    * chore: run tests on Windows as well for GA
    
    * fix(tests): set USERPROFILE for tilde expansion test on Windows
    
    * avoid setting QT offscreen variable on Windows
    
    * ok, we actually do need to setup a headless display

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 """Global fixtures shared across all tests."""
 
 import os
+import sys
 
 import numpy as np
 import pytest
@@ -8,7 +9,13 @@ import pytest
 # Use the offscreen Qt backend when no display is available (e.g. CI).
 # Must be set before any Qt import so that QApplication does not try to
 # connect to an X server and abort.
-os.environ.setdefault("QT_QPA_PLATFORM", "offscreen")
+# There is a mismatch between QT offscreen support and vispy OpenGL support on Windows
+# https://github.com/napari/napari/issues/5355
+# https://github.com/napari/napari/issues/804
+# On Windows CI, we setup the headless display on the GitHub Actions workflow
+if sys.platform != "win32":
+    # Linux/macOS CI usually has no display; use the offscreen Qt backend.
+    os.environ.setdefault("QT_QPA_PLATFORM", "offscreen")
 
 
 @pytest.fixture(scope="session")
