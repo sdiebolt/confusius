80c97c0953da450dcdbd3bec29345e9cb3f649b0
Samuel Le Meur-Diebolt <samuel@diebolt.io>
2026-05-12 18:15:59 +0100

    Revert "docs(plotting): add NumPy docstrings to new private helpers"
    This reverts commit e5c19f9cbf4a744daf452214fa57b23ee4a8d96f.

--- src/confusius/plotting/image.py
+++ src/confusius/plotting/image.py
@@ -2230,8 +2230,7 @@ def plot_carpet(
     -----
     Complex-valued data is converted to magnitude before processing.
 
-    This function was inspired by Nilearn's
-    [`nilearn.plotting.plot_carpet`][nilearn.plotting.plot_carpet].
+    This function was inspired by Nilearn's `nilearn.plotting.plot_carpet`.
 
     References
     ----------
